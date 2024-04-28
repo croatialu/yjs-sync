e838807abd4fde80842eb64ce2c00507b2b4c747
croatialu <wuli.croatia@foxmail.com>
2024-04-28 14:44:50 +0800

    update release setting

--- packages/y-vue/src/doc/context.ts
+++ packages/y-vue/src/doc/context.ts
@@ -2,14 +2,6 @@ import type * as Y from 'yjs'
 import { inject, provide } from 'vue-demi'
 import type { Provider } from './types'
 
-// export const DocumentContext = React.createContext<{
-//   doc: Y.Doc | null
-//   providers: Map<new (...args: any[]) => Provider, Map<string, Provider>> | null
-// }>({
-//       doc: null,
-//       providers: null,
-//     })
-
 const YJS_VUE_IDENTIFIER = 'YJS_VUE'
 
 interface DocumentContext {
